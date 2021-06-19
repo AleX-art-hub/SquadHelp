864ca2cf8db0b2029303e13f4c39d8cf639c406e
BodNS <bondarenokn@gmail.com>
2021-06-19 10:01:04 +0300

    fix all

--- server/src/seeders/123123123-Bank.js
+++ server/src/seeders/123123123-Bank.js
@@ -1,6 +1,8 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Banks', [
+    await queryInterface.bulkInsert(
+      'Banks', 
+      [
       {
         cardNumber: '4564654564564564',
         name: 'SquadHelp',
@@ -10,12 +12,22 @@ module.exports = {
       },
       {
         cardNumber: '4111111111111111',
-        name: 'yriy',
+        name: 'buyer',
         expiry: '09/23',
         cvc: '505',
         balance: 5000,
       },
+      {
+        cardNumber: '5111111111111111',
+        name: 'creator',
+        expiry: '09/23',
+        cvc: '506',
+        balance: 5,
+      },
     ], {});
+  },
+  up: async(queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Banks', null, {});
   },
 
 };
